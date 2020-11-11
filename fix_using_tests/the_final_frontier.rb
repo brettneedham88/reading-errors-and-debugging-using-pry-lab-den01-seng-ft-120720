3dc865d39f010c88d6a155182ea127f999e8956d
Brett Needham <brettneedham88@gmail.com>
2020-11-11 16:50:55 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,12 +1,13 @@
 require 'pry'
 
-def generate_star_date
+def generate_star_date(number)
   (rand(100000) + 400000) / 10.0
 end
 
 
 def state_log(date)
- 
+  puts state_log
+  date = generate_star_date
 end
 
 def engage(star_date)
