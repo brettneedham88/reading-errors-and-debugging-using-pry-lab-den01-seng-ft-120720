a3102f1468aa7a991801f8154920afb29b786fb8
Brett Needham <brettneedham88@gmail.com>
2020-11-11 17:22:04 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,8 +1,10 @@
 require 'pry'
 
-
+def generate_star_date
+  (rand(100000) + 400000) / 10.0
+end
 
 def engage(star_date)
- put "Captain's Log, star date #{star_date}."
+ puts "Captain's Log, star date #{star_date}."
 end
 
